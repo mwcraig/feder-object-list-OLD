51ba087dd34fc1971db818748b8ef2b62d490035
Matthew Craig <mattwcraig@gmail.com>
2017-06-01 17:31:11 -0500

    Increase default allowed width for a match because many objects are extended enough that the radius really ought to be bigger.

--- test_valid_object_file.py
+++ test_valid_object_file.py
@@ -13,7 +13,7 @@ class CoordinateMismatchError(Exception):
     pass
 
 TABLE_NAME = 'feder_object_list.csv'
-MAX_SEP = 5  # arcsec
+MAX_SEP = 30  # arcsec
 
 # increase timeout so that the Travis builds succeed
 utils.data.conf.remote_timeout = 30
